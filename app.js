295e0c68f7a6b4448cc5dc3a0b89dc6060486b18
meichholz <tim.eichholz98@web.de>
2019-05-13 12:27:15 +0200

    bootstrap

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require("express")();
+const express = require("express");
 const app = express();
 const router = require("./routes/router.js")
 
@@ -8,10 +8,10 @@ app.set('views', 'views');
 app.use(express.static("public"));
 app.use("/", router);
 // Letzte Middleware in der Kette. Behandelt den Fall das die Ressource nicht existiert
-app.use(function(req, res){
+/* app.use(function(req, res){
     res.render('404', {
         url: req.url
     });
-});
+}); */
 
 app.listen(8040, (err) => console.log("Server Lauscht auf localhost:8040"));
