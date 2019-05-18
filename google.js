5f7e90fe7c1efe9a4a7459b25f33016e99a421bc
meichholz <tim.eichholz98@web.de>
2019-05-18 21:25:28 +0200

    fetch + google api

--- google.js
+++ google.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const fsPromises = fs.promises;
+const fetch = require("cross-fetch").fetch;
 
 async function readGoogleApiKey() {
     let key;
@@ -12,6 +13,27 @@ async function readGoogleApiKey() {
     return key.google;
 }
 
-async function fetchGoogleResult(){
-
+async function fetchGoogleResult() {
+    const key = await readGoogleApiKey();
+    const url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?' +
+        'key=' + key +
+        '&input=Brandenburger%Tor&inputtype=textquery&fields=name';
+    let res;
+    try {
+        res = await fetch(url);
+    } catch (err) {
+        console.log("Fehler beim fetchen: " + err);
+    }
+    return res;
 }
+
+fetchGoogleResult()
+    .then((res) => {
+        return res.json();
+    })
+    .then((json) => {
+        console.log(json);
+    })
+    .catch((err) => {
+        console.log("fehler: " + err);
+    });
